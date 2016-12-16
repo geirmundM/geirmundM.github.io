b8485796d48d01e3214a92b7bf036744745ab884
geirmundM <geirmund.meland@gmail.com>
2016-12-16 13:49:38 +0100

    Minor adjustment y-axis

--- sinewave.js
+++ sinewave.js
@@ -35,7 +35,7 @@ function plotValues() {
 		child = parent.firstChild;
 	}	
 	// Add axis 
-	addAxis(xStart, yStart, 350, 225, "Time", "Amplitude"); 	// function addAxis(xPos, yPos, xLen, yLen)
+	addAxis(xStart, yStart, 350, 215, "Time", "Amplitude"); 	// function addAxis(xPos, yPos, xLen, yLen)
 	// Add new points in plot
 	for(var i = 0; i < yValues.length; i++) {
 		addPoint(xStart + 5 * i, yValues[i]);
